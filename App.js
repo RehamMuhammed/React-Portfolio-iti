dc2d2a373dee4242e847f81304ecd45cf2780e42
RehamMuhammed <rehammuhammedadel@gmail.com>
2023-08-16 21:37:17 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import { NavBar } from './components/NavBar';
+import { Banner } from './components/Banner';
+import { Skills } from './components/Skills';
+import { Projects } from './components/Projects';
+import { ProjectCard } from './components/ProjectCard';
+import { Contact } from './components/Contact';
+import { Footer } from "./components/Footer";
+
+
+
+
+function App() {
+  return (
+    <div>
+     <NavBar/>
+     <Banner/>
+     <Skills/>
+     <Projects/>
+     <ProjectCard/>
+     <Contact/>
+     <Footer/>
+    </div>
+  );
+}
+
+export default App;
